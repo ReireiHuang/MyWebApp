0be3963d630b2ed78cb226701233fcbd1a89af89
Bill <bill25961073@gmail.com>
2021-08-09 14:44:27 +0800

    new WebApplication

---
+++ WebApplication.DataModel/DataModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication.DataModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
